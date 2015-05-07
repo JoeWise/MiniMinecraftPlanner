f5ce685b9c863d7fe6ff44fbee9f425e95b0f17f
Joe Wise <JosephWise93@gmail.com>
2015-05-07 00:24:13 -0700

    Changed state to be represented at a tuple of tuples. Each tuple in a state contains the name of an item and how much of that item is in the inventory, including 0 so every item is always in the state. The state starts off with all items at 0. Changed everything accordingly to work with this new representation. Implemented checker. Tested search on crafting a bench and it seems to work.

--- planner.py
+++ planner.py
@@ -30,37 +30,76 @@ from heapq import heappush, heappop
 #	'Time': 1
 # }
 
-class State:
+# class State:
+#
+#     def __init__(self, inventory, state=None):
+#         self.inventory = dict((el,0) for el in inventory)
+#         if (state is not None): self.inventory = state.inventory
+#
+#     def __hash__(self):
+#         h = frozenset(self.inventory.items()).__hash__()
+#         return h
+#
+#     def __eq__(self,other):
+#         pass
+#
+#     def has_items(self, goals):
+#         has_item = True
+#
+#         for item in goals:
+#             if (self.inventory[item] < goals[item]):
+#                 has_item = False
+#                 break
+#             else:
+#                 print "HAS A GOAL: ", item
+#
+#         return has_item
 
-    def __init__(self, inventory, state=None):
-        self.inventory = dict((el,0) for el in inventory)
-        if (state is not None): self.inventory = state.inventory
+def generate_initial_state():
+    return tuple((i,0) for i in Crafting['Items'])
 
-    def __hash__(self):
-        h = frozenset(self.inventory.items()).__hash__()
-        return h
-
-    def __eq__(self,other):
-        pass
-
-    def has_items(self, goals):
-        has_item = True
-        
-        for item in goals:
-            if (self.inventory[item] < goals[item]):
-                has_item = False
-                break
+def normalize_state(items):
+    new_state = []
+    for i in Crafting['Items']:
+            if i in items:
+                new_state.append((i, items[i]))
             else:
-                print "HAS A GOAL: ", item
+                new_state.append((i, 0))
+    return tuple(new_state)
 
-        return has_item
+def next_state(state, cons, prod):
+    newState = list(state)
+
+    for i in range(0, len(state)):
+    # for item in rule["Consumes"]:
+        newState[i] = (newState[i][0], newState[i][1] - cons[i][1])
+
+    #     newState.inventory[item] -= rule["Consumes"][item]
+        newState[i] = (newState[i][0], newState[i][1] + prod[i][1])
+    # for item in rule["Produces"]:
+    #     newState.inventory[item] += rule["Produces"][item]
+    return tuple(newState)
+
+def has_items(state, goals):
+    has_item = True
+    n_goals = normalize_state(goals)
+
+    for i in range(0, len(n_goals)):
+    # for item in goals:
+        if (state[i][1] < n_goals[i][1]):
+            has_item = False
+            break
+        else:
+            # print "HAS A GOAL: ", state[i]
+            pass
+    return has_item
 
     
 def make_goal_checker(goals):
     # this code runs once
     def is_goal(state, goals):
         # this code runs millions of times
-        return state.has_items(goals)
+        return has_items(state, goals)
         #return True # or False
 
     return is_goal
@@ -71,23 +110,23 @@ def make_checker(rule):
     # do something with rule['Consumes'] and rule['Requires']
     def check(state):
         satisfiesConsumes = True
-        satisifiesRequires = True
+        satisfiesRequires = True
         if 'Consumes' in rule.keys():
-            #satisfiesConsumes = state.has_items(rule["Consumes"])
-            pass
+            satisfiesConsumes = has_items(state, rule["Consumes"])
 
         if 'Produces' in rule.keys():
             
             pass
 
         if 'Requires' in rule.keys():
-            pass
+            satisfiesRequires = has_items(state, rule["Requires"])
         #print rule
         # this code runs millions of times
 
-        #return (satisfiesConsumes and satisifiesRequires) # or False
-        return True
+        return (satisfiesConsumes and satisfiesRequires) # or False
+
     return check
+
 
 def make_effector(rule):
     # this code runs once
@@ -95,31 +134,22 @@ def make_effector(rule):
     def effect(state):
         # this code runs millions of times
         #return next_state
-        newState = State({}, state)
-        for item in rule["Consumes"]:
-            newState.inventory[item] -= rule["Consumes"][item]
-
-        for item in rule["Produces"]:
-            newState.inventory[item] += rule["Produces"][item]
+        if 'Consumes' in rule:
+            newState = next_state(state, normalize_state(rule["Consumes"]), normalize_state(rule["Produces"]))
+        else:
+            newState = next_state(state, generate_initial_state(), normalize_state(rule["Produces"]))
 
         return newState
 
     return effect
 
-def heuristic():
+
+def t_heuristic(state):
     return 0
 
 
-Recipe = namedtuple('Recipe',['name','check','effect','cost'])
-all_recipes = []
-for name, rule in Crafting['Recipes'].items():
-    checker = make_checker(rule)
-    effector = make_effector(rule)
-    recipe = Recipe(name, checker, effector, rule['Time'])
-    all_recipes.append(recipe)
-
 def graph(state):
-  print "Graph called"
+  # print "Graph called"
 
   for r in all_recipes:
     if r.check(state):
@@ -127,7 +157,6 @@ def graph(state):
 
 
 def search(graph, initial, is_goal, limit, heuristic):
-
 
     prev = {initial: None}
     actions = {initial: None}
@@ -138,13 +167,13 @@ def search(graph, initial, is_goal, limit, heuristic):
     while q:
 
         node = heappop(q)
-        print "I: ", initial
-        print "I2: ", node[1]
+        # print "I: ", initial
+        # print "I2: ", node[1]
         if node[0] > limit:
             return float('inf'), []
 
         closed_set.append(node[1])
-        print "N: "
+        # print "N: "
         if is_goal(node[1], Crafting["Goal"]):
             plan = []
             total_cost = 0
@@ -164,26 +193,36 @@ def search(graph, initial, is_goal, limit, heuristic):
         #print(node[1])
 
         for next_node in neighbors:
-            print "NN:", next_node[0]
+            # print "NN:", next_node[0]
             if next_node[1] in closed_set:
                 continue
 
             action, next_state, cost = next_node
-            print "N1: ", action
-            print "TIMES: ", times
+            # print "N1: ", action
+            # print "TIMES: ", times
             cost_to_next_node = times[node[1]] + cost
 
             if next_state not in q or cost_to_next_node < times[next_state]:
                 prev[next_state] = node[1]
                 actions[next_state] = action
                 times[next_state] = cost + times[node[1]]
-                heappush(q, (heuristic(next_state) + cost_to_next_node, next_state))
+                priority = heuristic(next_state) + cost_to_next_node
+                heappush(q, (priority, next_state))
 
 
 
-initial_state = State(Crafting["Items"])
+Recipe = namedtuple('Recipe',['name','check','effect','cost'])
+all_recipes = []
+for name, rule in Crafting['Recipes'].items():
+    checker = make_checker(rule)
+    effector = make_effector(rule)
+    recipe = Recipe(name, checker, effector, rule['Time'])
+    all_recipes.append(recipe)
+
+# initial_state = State(Crafting["Items"])
+initial_state = generate_initial_state()
 is_goal = make_goal_checker(Crafting["Goal"])
-t_heuristic = heuristic()
+# t_heuristic = heuristic()
 t_limit = 30
 
 print "SEARCH: ", search(graph, initial_state, is_goal, t_limit, t_heuristic)
